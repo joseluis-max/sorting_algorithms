28a2753cea3165e2510b9b1ac48e164c742328a0
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-10-21 18:44:36 -0500

    all

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,1 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "sort.h"
 
+void bubble_sort(int *array, size_t size)
+{       
+        size_t k, i;
+        int tmp;
+        int flag = 0;
+
+        for (k = 0; k < size - 1; k++)
+        {
+                for (i = 0; i < size - 1 - k; i++)
+                {
+                        if (array[i] > array[i + 1])
+                        {
+                                tmp = array[i];
+                                array[i] = array[i + 1];
+                                array[i + 1] = tmp;
+                                flag = 1;
+                                print_array(array, size);
+                        }
+                }
+                if (flag == 0)
+                        return;
+        }
+}
